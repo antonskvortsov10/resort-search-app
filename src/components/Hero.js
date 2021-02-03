069e5be9e34ad9bdd0bdfd89e6a1fe4b612cfa22
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2021-02-03 19:47:00 +0300

    Updated the components

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Hero({childern, hero}) {
+    return (
+        <header className={hero}>
+            {childern}
+        </header>
+    )
+}
+
+Hero.defaultProps = {
+    hero: 'defaultHero'
+}
